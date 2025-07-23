e322c491e0eba1046074aae81f2dc0ddbc1792a5
pouyan nazarzade <poyannazarzade@gmail.com>
2025-07-23 15:59:12 -0400

    added the api folder

---
+++ dusic/api/callback.js
@@ -0,0 +1,26 @@
+export default async function handler(req, res) {
+  const code = req.query.code;
+
+  const client_id = process.env.SPOTIFY_CLIENT_ID;
+  const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
+  const redirect_uri = 'https://dusic-5acv.vercel.app/callback'; // must match Spotify settings
+
+  const response = await fetch('https://accounts.spotify.com/api/token', {
+    method: 'POST',
+    headers: {
+      Authorization:
+        'Basic ' +
+        Buffer.from(`${client_id}:${client_secret}`).toString('base64'),
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: new URLSearchParams({
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: redirect_uri,
+    }),
+  });
+
+  const data = await response.json();
+  console.log('Spotify token response:', data);
+  res.status(200).json(data);
+}
