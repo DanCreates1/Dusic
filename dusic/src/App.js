be0921d9f15fcd3642fa7312e6079969dfe63a80
pouyan nazarzade <poyannazarzade@gmail.com>
2025-07-23 16:32:27 -0400

    finial push

--- dusic/src/App.js
+++ dusic/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import NowPlaying from './nowPlaying.jsx';  
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to Dusic!</h1>
-    
+    <nowPlaying/>
      <p>Made by Daniel</p>
     </div>
   );
